d8e219d7c2fbf7908ab1d93181ea8320e9622b3f
elisesb <elisebratveit@outlook.com>
2025-02-17 17:50:15 +0100

    init commit

---
+++ src/main/java/com/example/verifiserer/VerifisererApplication.java
@@ -0,0 +1,13 @@
+package com.example.verifiserer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifisererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifisererApplication.class, args);
+	}
+
+}
