d3b1955d5826a5d63beae6d2bac1f310ee7852df
elisesb <elisebratveit@outlook.com>
2025-03-05 15:30:34 +0100

    legger til en test for callbacken

--- src/main/java/com/example/verifiserer/controller/ResponseController.java
+++ src/main/java/com/example/verifiserer/controller/ResponseController.java
@@ -1,20 +1,22 @@
 package com.example.verifiserer.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/verifisere")
 public class ResponseController {
 
+    String test;
+
     @PostMapping("/callback")
     public String callBack(@RequestBody String requestBody){
+        test = requestBody;
         System.out.println("Request body" + requestBody);
         return "Callback successfully";
-
     }
 
-
+    @GetMapping("/testerCallbacken")
+    public String getTestValue() {
+        return test;
+    }
 }
