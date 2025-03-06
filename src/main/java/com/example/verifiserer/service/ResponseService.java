04a21c3e104f24335ee3dad8e374e14a5aee3a3e
elisesb <elisebratveit@outlook.com>
2025-03-06 11:52:42 +0100

    test: jwt-token

--- src/main/java/com/example/verifiserer/service/ResponseService.java
+++ src/main/java/com/example/verifiserer/service/ResponseService.java
@@ -1,4 +1,14 @@
 package com.example.verifiserer.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ResponseService {
+
+    public String getJustToken(String text){
+        return text.split("vp_token=")[1];
+
+    }
+
+
 }
